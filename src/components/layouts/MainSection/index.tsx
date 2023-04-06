6d6dd97e25741df047e3472a97c0ef23b8f7183f
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-04-06 19:18:13 -0300

    :sparkles: feat: adicionando o hovertree

---
+++ src/components/layouts/MainSection/index.tsx
@@ -0,0 +1,59 @@
+import "./MainSection.css";
+import "./hovertree.js";
+
+import { motion } from "framer-motion";
+
+export function MainSection() {
+  return (
+    <main>
+      <div className="text">
+        <motion.span
+                    animate={{ y: 50 }}
+                    transition={{ ease: "linear", stiffness: 100, delay: 10 }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    viewport={{ once: true }}
+          className="companyName"
+        >
+          Monopolius SA
+        </motion.span>
+        <motion.h1
+          className="title"
+          animate={{ y: 50 }}
+          transition={{ ease: "linear", stiffness: 100, delay: 9 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          NÓS SOMOS O <br />
+          <u className="sub">FUTURO</u> DA <br />
+          <u className="sub">TECNOLOGIA</u>
+        </motion.h1>
+        <motion.span
+          className="subtitle"
+          animate={{ y: 50 }}
+          transition={{ ease: "linear", stiffness: 100, delay: 8 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          Que tal embarcar no futuro?
+        </motion.span>
+
+        <motion.a
+          href="#"
+          className="btn btn1"
+          animate={{ y: 50 }}
+          transition={{ ease: "linear", stiffness: 100, delay: 7 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          SIGA-NOS NO INSTAGRAM!
+        </motion.a>
+      </div>
+
+      <canvas id="canvas"></canvas>
+    </main>
+  );
+}
