cad21253ef47fe2df2865dc3d5192edf4f816d4b
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-04-08 18:50:31 -0300

    :sparkles: feat: Criando o footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,35 @@ import { Loading } from "./components/Loading";
 import { Header } from "./components/Header";
 import { MainSection } from "./components/layouts/MainSection";
 import { AboutSection } from "./components/layouts/AboutSection";
+import { PerrynaitorSection } from "./components/layouts/PerrynaitorSection";
+import { QuestionsSection } from "./components/layouts/QuestionsSection";
+import { OpinionsSection } from "./components/layouts/OpinionsSection";
+import { ContactUsSection } from "./components/layouts/ContactUsSection";
+
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="content">
+
 
       <MainSection />
       <AboutSection />
-      <Loading /> 
+      <PerrynaitorSection />
+      <QuestionsSection />
+      <OpinionsSection />
+      </div>
+
+      <ContactUsSection />
+      {/*  <Loading />  */}
+
+      <footer>
+        <p>
+          Monoplius SA <span>&#169;</span> Todos os direitos reservados.
+        </p>
+      </footer>
     </div>
   );
 }
