16ef1fb3e9d80adc724f6e5f955173ea54aeff00
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-04-08 15:42:08 -0300

    :sparkles: feat: Criando a Section FAC

---
+++ src/components/layouts/QuestionsSection/index.tsx
@@ -0,0 +1,27 @@
+import AccordionComponent from "../../Accordion";
+import "./QuestionsSection.css";
+
+export function QuestionsSection() {
+  return (
+    <div className="facContainer">
+      <div className="facHeader">
+        <h6>03.</h6>
+        <u>Perguntas frequentes</u>
+      </div>
+
+      <div className="facContent">
+        <div className="colLeft">
+          <p>
+            Está com alguma dúvida em relação ao Perrynaitor ou da nossa
+            empresa? Veja as dúvidas mais frequentes, caso não encontre o que
+            deseja, por favor entre em contato.
+          </p>
+        </div>
+
+        <div className="colRight">
+          <AccordionComponent />
+        </div>
+      </div>
+    </div>
+  );
+}
